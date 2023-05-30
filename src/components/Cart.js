e05f90d9e0f2739cfa831b4399277868a504e75d
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-30 15:41:31 +0200

    components

---
+++ src/components/Cart.js
@@ -0,0 +1,20 @@
+function Cart() {
+  const monstreraPrice = 8;
+  const lierrePrice = 10;
+  const bouquetPrice = 15;
+
+  return (
+    <div>
+      <h2>Panier</h2>
+      <ul>
+        <li>Monstera : {monstreraPrice}€</li>
+        <li>Lierre : {lierrePrice}€</li>
+        <li>Bouquet : {bouquetPrice}€</li>
+      </ul>
+
+      <p>Total : {monstreraPrice + lierrePrice + bouquetPrice}€</p>
+    </div>
+  );
+}
+
+export default Cart;
