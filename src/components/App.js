8fcab1bbc77e2e2804bbc0cb3a06ba50410dd46a
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-30 16:36:33 +0200

    key

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Banner from "./Banner.js";
 import Cart from "./Cart.js";
+import ShoppingList from "./ShoppingList.js";
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   );
 }
