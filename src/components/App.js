b20de547b852841f370a7a53151e133c683e0669
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-31 11:40:43 +0200

    footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import Banner from "./Banner.js";
 import Cart from "./Cart.js";
 import ShoppingList from "./ShoppingList.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <Cart />
+      {/*<Cart />*/}
       <ShoppingList />
+      <Footer />
     </div>
   );
 }
